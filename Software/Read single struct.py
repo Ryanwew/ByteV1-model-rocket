86be997af73954182bab648cdddcb5d97b3cb500
Ryan Dick <ryanjamesdick@gmail.com>
2021-04-27 14:56:45 -0400

    Finished test parse app

--- Software/Read single struct.py
+++ Software/Read single struct.py
@@ -3,16 +3,35 @@ import csv
 
 lsdata = []
 
-with open("A:\\GithubDesktop\\Model-Rocket-Design\\Software\\X.DAT", "rb") as dataFile:
-    for x in range(50):
-        b = dataFile.read(4)
-        a = struct.unpack('f', b)
-        lsdata.append(a)
-
-
+# Clears any previous iterations and adds headers to the CSV file
 with open("C:\\Users\\Ryan\\Desktop\\Byte drop\\dataout.csv", "w", newline='') as newFile:
     csvWriter = csv.writer(newFile, delimiter=",")
-    for y in lsdata:
-        csvWriter.writerow(y)
+    newFile.truncate()
+    csvWriter.writerow(["time", "data", "state"])
+    newFile.close()
 
+# Loops through the DAT bin file untill there is no data, converts each value into the appropriate variable and appends them to the CSV
+with open("A:\\GithubDesktop\\Model-Rocket-Design\\Software\\X2.DAT", "rb") as dataFile:
+    while True:
+        b = dataFile.read(4)
+        if not b:
+            break
+        a = struct.unpack('L', b)
+        lsdata.append(a[0])
+        b = dataFile.read(4)
+        if not b:
+            break
+        a = struct.unpack('f', b)
+        lsdata.append(a[0])
+        b = dataFile.read(1)
+        if not b:
+            break
+        lsdata.append(int.from_bytes(b, "big"))
+        with open("C:\\Users\\Ryan\\Desktop\\Byte drop\\dataout.csv", "a", newline='') as newFile:
+            csvWriter = csv.writer(newFile, delimiter=",")
+            csvWriter.writerow(lsdata)
+            lsdata.clear()
+
+
+print(lsdata)
 print("done!")
